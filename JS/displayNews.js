8bf984236951607d59cd46d7ce6ebf098149e62b
Dragomir Ionut <ionutdragomir1410@yahoo.com>
2022-02-12 12:35:06 +0200

    changed API

--- JS/displayNews.js
+++ JS/displayNews.js
@@ -1,7 +1,7 @@
 export function displayNews(allNews) {
-      const template = document.querySelector("[data-news-template]");
-      const newsBody = document.querySelector("[data-newsBody]");
-      newsBody.innerHTML = `<template data-news-template>
+  const template = document.querySelector("[data-news-template]");
+  const newsBody = document.querySelector("[data-newsBody]");
+  newsBody.innerHTML = `<template data-news-template>
       <div class="news" data-news-card>
         <p id="date" data-news-date></p>
         <p id="title" data-news-title></p>
@@ -10,28 +10,28 @@ export function displayNews(allNews) {
         <a href="" id="link" data-news-link>Go to News.</a>
       </div>
     </template>`;
-    
-      allNews.forEach((article) => {
-        let cardContent = template.content.cloneNode(true);
-    
-        let date = cardContent.querySelector("[data-news-date]");
-        let formatDate;
-        let title = cardContent.querySelector("[data-news-title]");
-        let image = cardContent.querySelector("[data-news-image]");
-        let content = cardContent.querySelector("[data-news-content]");
-        let link = cardContent.querySelector("[data-news-link]");
-        if (article.API === 1) {
-          formatDate = article.published_at.split("T");
-          image.src = article.image_url;
-        } else {
-          formatDate = article.datePublished.split("T");
-          image.src = article.image.url;
-        }
-    
-        date.innerText = formatDate[0];
-        title.innerText = article.title;
-        content.innerText = article.description;
-        link.href = article.url;
-        newsBody.append(cardContent);
-      });
+
+  allNews.forEach((article) => {
+    let cardContent = template.content.cloneNode(true);
+
+    let date = cardContent.querySelector("[data-news-date]");
+    let formatDate;
+    let title = cardContent.querySelector("[data-news-title]");
+    let image = cardContent.querySelector("[data-news-image]");
+    let content = cardContent.querySelector("[data-news-content]");
+    let link = cardContent.querySelector("[data-news-link]");
+    if (article.API === 1) {
+      formatDate = article.published_at.split("T");
+      image.src = article.image_url;
+    } else {
+      formatDate = article.datePublished.split("T");
+      image.src = article.image.url;
     }
+
+    date.innerText = formatDate[0];
+    title.innerText = article.title;
+    content.innerText = article.description;
+    link.href = article.url;
+    newsBody.append(cardContent);
+  });
+}
