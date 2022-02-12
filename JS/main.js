80da3b9c1b0c2a16fa960dc4b2bf44dc36fb4137
Dragomir Ionut <ionutdragomir1410@yahoo.com>
2022-02-12 10:58:12 +0200

    update main file to new API

--- JS/main.js
+++ JS/main.js
@@ -3,13 +3,13 @@ import { displayNews } from "./displayNews.js";
 import { sortNews } from "./sortNews.js";
 
 document.querySelector("[data-button]").addEventListener("click", main);
-window.addEventListener("load", main);
+// window.addEventListener("load", main);
 
 async function main() {
   let search = document.querySelector("[data-input]").value;
-  if (search === "") search = "latest%20news";
-  console.log("search value:", search);
+  // if (search === "") search = "latest%20news";
   let result = await getNews(search);
-  console.log("result value:", result);
+  console.log(result);
+  // console.log(sortNews(result[0], result[1]));
   displayNews(sortNews(result[0], result[1]));
 }
